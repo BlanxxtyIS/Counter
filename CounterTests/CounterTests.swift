6807b57e6e5c6e237f60a2d2d0fac9e1c32c315e
BlanxxtyIS <Blanxxtyios@mail.ru>
2023-06-14 21:27:44 +0300

    Добавил функцию вывода текста на экран в CounterTest

--- CounterTests/CounterTests.swift
+++ CounterTests/CounterTests.swift
@@ -12,6 +12,7 @@ final class CounterTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("Hello, Test")
     }
 
     override func tearDownWithError() throws {
